32cb77f265f89bb0322a9065acb1c99c210573a9
Khaleda Khan <khaleda.khan87@gmail.com>
2016-07-12 15:53:07 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,25 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_org_by_name = {}
+    data.each do |attributes,details|
+      details.each do |categories,birds|
+      	birds.each do |bird|
+       pigeon_org_by_name[bird] = {:color => [], :lives => [], :gender => []
+       }
+       end
+      end
+      end
+      data.each do |attributes,details|
+      details.each do |categories,birds|
+      	birds.each do |bird|
+      		if attributes == :color
+       	pigeon_org_by_name[bird][:color] << categories.to_s
+       	elsif attributes == :lives
+       	pigeon_org_by_name[bird][:lives] << categories.to_s
+       	elsif attributes == :gender
+       	pigeon_org_by_name[bird][:gender] << categories.to_s
+       end
+   end
+    end
+    end
+    return pigeon_org_by_name
 end
